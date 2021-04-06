3aae9950c087c47479acb517e371f0a7765efb7d
ATM john <john.botella@atm-consulting.fr>
2021-04-06 17:24:45 +0200

    FIX : Dolibarr V13 token compatibility

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,5 +1,8 @@
 # CHANGELOG FIXEDTABLEHEAD FOR <a href="https://www.dolibarr.org">DOLIBARR ERP CRM</a>
 
 ## 1.0
+
+- FIX : Dolibarr V13 token compatibility *06/04/2021* - 1.0.1
+
 Initial version
 
--- js/fixedtablehead.js.php
+++ js/fixedtablehead.js.php
@@ -22,12 +22,11 @@
 //if (!defined('NOREQUIRESOC'))   define('NOREQUIRESOC', '0');
 //if (!defined('NOREQUIRETRAN'))  define('NOREQUIRETRAN','1');
 //if (!defined('NOCSRFCHECK'))    define('NOCSRFCHECK', 1);
-//if (!defined('NOTOKENRENEWAL')) define('NOTOKENRENEWAL', 1);
+if (!defined('NOTOKENRENEWAL')) define('NOTOKENRENEWAL', 1);
 //if (!defined('NOLOGIN'))        define('NOLOGIN', 1);
 //if (!defined('NOREQUIREMENU'))  define('NOREQUIREMENU', 1);
 //if (!defined('NOREQUIREHTML'))  define('NOREQUIREHTML', 1);
 //if (!defined('NOREQUIREAJAX'))  define('NOREQUIREAJAX','1');
-
 
 /**
  * \file    fixedtablehead/js/fixedtablehead.js.php
